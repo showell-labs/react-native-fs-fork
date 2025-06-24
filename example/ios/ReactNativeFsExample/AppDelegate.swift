7044e524048857880bd634185575919905447738
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-06-24 23:26:18 +0200

    [#113] v2.34.0: Upgrade to RN@0.80

--- example/ios/ReactNativeFsExample/AppDelegate.swift
+++ example/ios/ReactNativeFsExample/AppDelegate.swift
@@ -6,10 +6,10 @@ import ReactAppDependencyProvider
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
- 
+
   var reactNativeDelegate: ReactNativeDelegate?
   var reactNativeFactory: RCTReactNativeFactory?
- 
+
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
@@ -17,18 +17,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
- 
+
     reactNativeDelegate = delegate
     reactNativeFactory = factory
- 
+
     window = UIWindow(frame: UIScreen.main.bounds)
- 
+
     factory.startReactNative(
       withModuleName: "ReactNativeFsExample",
       in: window,
       launchOptions: launchOptions
     )
- 
+
     return true
   }
 }
