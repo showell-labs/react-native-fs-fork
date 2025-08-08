74d551fdea09ffb5f1c80a31ee92466053651d2b
Tero Paananen <tero.paananen@showell.com>
2025-08-08 12:55:47 +0300

    Revert changes

--- example/android/app/src/main/java/drpogodin/reactnativefs/example/MainApplication.kt
+++ example/android/app/src/main/java/drpogodin/reactnativefs/example/MainApplication.kt
@@ -4,14 +4,11 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
-
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-
-import com.facebook.soloader.SoLoader
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 
 class MainApplication : Application(), ReactApplication {
 
@@ -36,6 +33,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
+    loadReactNative(this)
   }
 }
